28e336e0540f0416496d1b9ef80d506995f402d2
Kelly Davidson <kellyadavidsonuk@gmail.com>
2019-07-10 19:56:04 +0100

    removed unnecessary code from files

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,60 +1,3 @@
-// 'use strict';
-
-// import React, { Component } from 'react';
-
-// import {StyleSheet} from 'react-native';
-
-// import {
-//   ViroARScene,
-//   ViroText,
-//   ViroConstants,
-// } from 'react-viro';
-
-// export default class HelloWorldSceneAR extends Component {
-
-//   constructor() {
-//     super();
-
-//     // Set initial state here
-//     this.state = {
-//       text : "Initializing AR..."
-//     };
-
-//     // bind 'this' to functions
-//     this._onInitialized = this._onInitialized.bind(this);
-//   }
-
-//   render() {
-//     return (
-//       <ViroARScene onTrackingUpdated={this._onInitialized} >
-//         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-//       </ViroARScene>
-//     );
-//   }
-
-//   _onInitialized(state, reason) {
-//     if (state == ViroConstants.TRACKING_NORMAL) {
-//       this.setState({
-//         text : "Hello World!"
-//       });
-//     } else if (state == ViroConstants.TRACKING_NONE) {
-//       // Handle loss of tracking
-//     }
-//   }
-// }
-
-// var styles = StyleSheet.create({
-//   helloWorldTextStyle: {
-//     fontFamily: 'Arial',
-//     fontSize: 30,
-//     color: '#ffffff',
-//     textAlignVertical: 'center',
-//     textAlign: 'center',
-//   },
-// });
-
-// module.exports = HelloWorldSceneAR;
-
 "use strict";
 
 import React, { Component } from "react";
@@ -71,12 +14,6 @@ import {
   ViroARSceneNavigator,
   ViroText
 } from "react-viro";
-
-// import {
-//   ViroARScene,
-//   ViroText,
-//   ViroConstants,
-// } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
   constructor() {
@@ -113,13 +50,13 @@ export default class HelloWorldSceneAR extends Component {
   }
 }
 
-_handleButtonPress() {
-  return () => {
-    this.setState({
-      navigatorType: navigatorType
-    });
-  };
-}
+// _handleButtonPress() {
+//   return () => {
+//     this.setState({
+//       navigatorType: navigatorType
+//     });
+//   };
+// }
 
 ViroARTrackingTargets.createTargets({
   targetOne: {
